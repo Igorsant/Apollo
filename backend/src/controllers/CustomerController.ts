a7d2ea7830acf991efb201f2de603d6034d6053c
benjamimFG <ben.freitasg@gmail.com>
2022-04-13 16:06:54 -0300

    Adiciona lógica para registrar clientes
    Adiciona a dependência bcryptjs para criar hash dos passwords
    
    Cria o método register no CustomerController para realizar o cadastro de clientes

---
+++ backend/src/controllers/CustomerController.ts
@@ -0,0 +1,64 @@
+import bcrypt from 'bcryptjs';
+import { Request, Response } from 'express';
+
+import { cpfIsValid } from '../helpers/cpf.helper';
+import databaseService from '../services/DatabaseService';
+import { saveImageFromBase64 } from '../helpers/image.helper';
+
+export default class CustomerController {
+  public static async register(req: Request, res: Response) {
+    const customer = req.body;
+
+    if (!cpfIsValid(customer.cpf))
+      return res.status(400).json({ error: `cpf ${customer.cpf} é inválido.` });
+
+    let picturePath = '/pictures/default_user.jpg';
+
+    if (customer.pictureBase64) {
+      try {
+        picturePath = await saveImageFromBase64(customer.pictureBase64, 'jpg');
+      } catch (err) {
+        console.error(err);
+        return res
+          .status(500)
+          .json({ error: 'Erro interno ao salvar imagem.' });
+      }
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(customer.password, salt);
+    delete customer.password;
+
+    databaseService.connection.transaction(async (trx) => {
+      try {
+        const [insertedPhone] = await trx('phone')
+          .insert({
+            phone: customer.phone
+          })
+          .returning('id');
+
+        await trx('customer').insert({
+          full_name: customer.fullName,
+          nickname: customer.nickname,
+          picture_path: picturePath,
+          email: customer.email,
+          phone_id: insertedPhone.id,
+          cpf: customer.cpf,
+          password_hash: hashedPassword
+        });
+
+        await trx.commit();
+      } catch (err) {
+        console.error(err);
+
+        return res
+          .status(500)
+          .json('Erro ao inserir usuário no banco de dados');
+      }
+    });
+
+    customer.picturePath = picturePath;
+
+    return res.status(201).json(customer);
+  }
+}

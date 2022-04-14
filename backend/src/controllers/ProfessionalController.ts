dd43f0d6ee6f4a906d75c5653b7cf17c4e5281ed
PauloUbirajara <ubirajara3@hotmail.com>
2022-04-14 20:24:22 -0300

    Criar método no controller para cadastrar profissional

--- backend/src/controllers/ProfessionalController.ts
+++ backend/src/controllers/ProfessionalController.ts
@@ -4,19 +4,22 @@ import { Request, Response } from 'express';
 import { cpfIsValid } from '../helpers/cpf.helper';
 import databaseService from '../services/DatabaseService';
 import { saveImageFromBase64 } from '../helpers/image.helper';
+import { insertPhone } from '../helpers/professional.helper';
+import { ServiceType } from '../types/service.type';
+import { WorkHourType } from '../types/workhour.type';
 
 const defaultPicturePath = '/pictures/default_user.jpg';
 
 export default class ProfessionalController {
   public static async register(req: Request, res: Response) {
     const professional = req.body;
+    let picturePath = '';
 
     if (!cpfIsValid(professional.cpf))
       return res
         .status(400)
         .json({ error: `cpf ${professional.cpf} é inválido.` });
 
-    let picturePath = null;
 
     if (professional.pictureBase64) {
       try {
@@ -36,84 +39,91 @@ export default class ProfessionalController {
     const hashedPassword = await bcrypt.hash(professional.password, salt);
     delete professional.password;
 
-    databaseService.connection.transaction(async (trx) => {
-      try {
-        const {
-          aboutMe: about_me,
-          cpf,
-          email,
-          fullName: full_name,
-          nickname,
-          phone,
-          picturePath: picture_path,
-          services,
-          workHours: work_hours,
-          workplace
-        } = professional;
+    return databaseService.connection
+      .transaction(async (trx) => {
+        const { workplace, services, workHours } = professional;
 
-        const {
-          complement,
-          isPhone1Whatsapp,
-          isPhone2Whatsapp,
-          phone1,
-          phone2,
-          street,
-          streetNumber: street_number
-        } = workplace;
-
-        const [insertedPhone1] = await trx('phone')
-          .insert({ phone: phone1, is_phone_whatsapp: isPhone1Whatsapp })
-          .returning('id');
+        const [insertedPhone1] = await insertPhone(trx, {
+          phone: workplace.phone1,
+          is_phone_whatsapp: workplace.isPhone1Whatsapp
+        }).returning('id');
 
         let insertedPhone2;
 
-        if (phone2) {
-          [insertedPhone2] = await trx('phone')
-            .insert({ phone: phone2, is_phone_whatsapp: isPhone2Whatsapp })
-            .returning('id');
+        if (workplace.phone2) {
+          [insertedPhone2] = await insertPhone(trx, {
+            phone: workplace.phone2,
+            is_phone_whatsapp: workplace.isPhone2Whatsapp
+          }).returning('id');
         }
 
         const [insertedWorkplace] = await trx('workplace')
           .insert({
-            street,
-            street_number,
-            complement,
+            street: workplace.street,
+            street_number: workplace.streetNumber,
+            complement: workplace.complement,
             phone1_id: insertedPhone1.id,
             phone2_id: insertedPhone2?.id
           })
           .returning('id');
 
-        const [insertedPhone] = await trx('phone')
-          .insert({ phone })
+        const [insertedPhone] = await insertPhone(trx, {
+          phone: professional.phone
+        }).returning('id');
+
+        const [insertedProfessional] = await trx('professional')
+          .insert({
+            about_me: professional.aboutMe,
+            cpf: professional.cpf,
+            email: professional.email,
+            full_name: professional.fullName,
+            nickname: professional.nickname,
+            password_hash: hashedPassword,
+            phone_id: insertedPhone.id,
+            picture_path: picturePath,
+            workplace_id: insertedWorkplace.id
+          })
           .returning('id');
 
-        await trx('professional').insert({
-          about_me,
-          cpf,
-          email,
-          full_name,
-          nickname,
-          password_hash: hashedPassword,
-          phone_id: insertedPhone.id,
-          picture_path,
-          services,
-          work_hours,
-          workplace: insertedWorkplace.id
-        });
+        await trx('service').insert(
+          services.map((service: ServiceType) => {
+            return {
+              name: service.name,
+              price: service.startingPrice,
+              estimated_duration: service.estimatedTime,
+              professional_id: insertedProfessional.id
+            };
+          })
+        );
+
+        await trx('workday').insert(
+          workHours.map((workHour: WorkHourType) => {
+            return {
+              start_time: workHour.startTime,
+              end_time: workHour.endTime,
+              break_time: false,
+              week_day: workHour.weekday,
+              professional_id: insertedProfessional.id
+            };
+          })
+        );
 
         await trx.commit();
-      } catch (err) {
+      })
+      .then((_) => {
+        professional.picturePath = picturePath || defaultPicturePath;
+
+        console.log('nao parei!!');
+        return res.status(201).json(professional);
+      })
+      .catch((err) => {
         console.error(err);
+        console.log('morri, devia parar!!');
 
         return res
           .status(500)
           .json('Erro ao inserir usuário no banco de dados');
-      }
-    });
-
-    professional.picturePath = picturePath || defaultPicturePath;
-
-    return res.status(201).json(professional);
+      });
   }
 }
 

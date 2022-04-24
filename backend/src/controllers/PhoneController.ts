2624a4c86672074924904fb326894486e4f24515
benjamimFG <ben.freitasg@gmail.com>
2022-04-24 00:25:05 -0300

    Adiciona método getPhonesByIds
    Adiciona método getPhonesByIds no PhoneController

--- backend/src/controllers/PhoneController.ts
+++ backend/src/controllers/PhoneController.ts
@@ -1,5 +1,5 @@
 import { Knex } from 'knex';
-import { toSnake } from 'snake-camel';
+import { toCamel, toSnake } from 'snake-camel';
 
 import databaseService from '../services/DatabaseService';
 import PhoneType from '../types/phone.type';
@@ -25,5 +25,13 @@ export default class PhoneController {
       .where('id', id);
 
     return { phone, isPhoneWhatasapp: is_phone_whatsapp };
+  }
+
+  public static async getPhonesByIds(ids: number[]) {
+    return databaseService.connection
+      .table('phone')
+      .select('phone', 'is_phone_whatsapp')
+      .whereIn('id', ids)
+      .then((rows) => rows.map(toCamel));
   }
 }

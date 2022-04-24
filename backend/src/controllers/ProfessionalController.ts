9bcde80d46dcdb4c7072d29b76147120ce9242f9
benjamimFG <ben.freitasg@gmail.com>
2022-04-24 00:36:53 -0300

    Refatora ProfessionalController
    Separa a inserção do banco em um método separado que é chamada no método register
    
    Separa o select de profissionais, tanto por email quanto por cidade em métodos separados ao login e search
    
    Para de acessar o banco diretamente nos métodos e usa os controllers referentes ao recurso a ser acessado
    
    Para de converter manualmente de snake_case para camelCase e vice versa e utiliza a biblioteca snake-case

--- backend/src/controllers/ProfessionalController.ts
+++ backend/src/controllers/ProfessionalController.ts
@@ -1,233 +1,108 @@
 import bcrypt from 'bcryptjs';
 import { Request, Response } from 'express';
-import fs from 'fs';
 import jwt from 'jsonwebtoken';
-import path from 'path';
+import { toCamel, toSnake } from 'snake-camel';
 
-import {
-  DEFAULT_USER_PICTURE,
-  PICTURES_FOLDER,
-  PICTURES_PATH
-} from '../helpers/consts.helper';
 import { cpfIsValid } from '../helpers/cpf.helper';
 import databaseService from '../services/DatabaseService';
-import { saveBase64Image, userPicture } from '../helpers/image.helper';
-import { insertPhone } from '../helpers/professional.helper';
-import ServiceController from './ServicesController';
-import { ServiceType } from '../types/service.type';
-import { WorkHourType } from '../types/workhour.type';
-import WorkHoursController from './WorkHoursController';
+import {
+  deletePicture,
+  saveBase64Image,
+  userPicture
+} from '../helpers/image.helper';
+import { badRequest, conflict, internalError } from '../helpers/http.helper';
+import PhoneController from './PhoneController';
+import ServiceController from './ServiceController';
+import ServiceType from '../types/service.type';
 import WorkplaceController from './WorkplaceController';
+import WorkHourController from './WorkHourController';
+import WorkHourType from '../types/workHour.type';
 
 export default class ProfessionalController {
   public static async register(req: Request, res: Response) {
     const professional = req.body;
 
     if (!cpfIsValid(professional.cpf))
-      return res
-        .status(400)
-        .json({ error: `cpf ${professional.cpf} é inválido.` });
+      return badRequest(res, `cpf ${professional.cpf} é inválido.`);
 
-    let pictureName = '';
-
-    if (professional.pictureBase64) {
-      try {
-        pictureName = await saveBase64Image(professional.pictureBase64, 'jpg');
-        delete professional.pictureBase64;
-      } catch (err) {
-        console.error(err);
-        return res
-          .status(500)
-          .json({ error: 'Erro interno ao salvar imagem.' });
-      }
+    try {
+      professional.pictureName = await saveBase64Image(
+        professional.pictureBase64
+      );
+      delete professional.pictureBase64;
+    } catch (err) {
+      console.error(err);
+      return internalError(res, 'Erro interno ao salvar imagem.');
     }
 
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(professional.password, salt);
+    professional.passwordHash = await bcrypt.hash(professional.password, salt);
     delete professional.password;
 
-    return databaseService.connection
-      .transaction(async (trx) => {
-        const { workplace, services, workHours } = professional;
+    try {
+      await ProfessionalController.insertProfessional(professional);
 
-        const [insertedPhone1] = await insertPhone(trx, {
-          phone: workplace.phone1,
-          is_phone_whatsapp: workplace.isPhone1Whatsapp
-        }).returning('id');
+      professional.picturePath = userPicture(professional.pictureName);
+      delete professional.pictureName;
+      delete professional.passwordHash;
 
-        let insertedPhone2;
+      return res.status(201).json(professional);
+    } catch (err) {
+      if (professional.pictureName) deletePicture(professional.pictureName);
 
-        if (workplace.phone2) {
-          [insertedPhone2] = await insertPhone(trx, {
-            phone: workplace.phone2,
-            is_phone_whatsapp: workplace.isPhone2Whatsapp
-          }).returning('id');
-        }
+      if (err.routine?.includes('unique')) {
+        const [_, key] = err.constraint.split('_');
 
-        const [insertedWorkplace] = await trx('workplace')
-          .insert({
-            city: workplace.city,
-            complement: workplace.complement,
-            phone1_id: insertedPhone1.id,
-            phone2_id: insertedPhone2?.id,
-            street_number: workplace.streetNumber,
-            street: workplace.street
-          })
-          .returning('id');
+        return conflict(res, key, professional[key]);
+      }
 
-        const [insertedPhone] = await insertPhone(trx, {
-          phone: professional.phone
-        }).returning('id');
+      console.error(err);
 
-        const [insertedProfessional] = await trx('professional')
-          .insert({
-            about_me: professional.aboutMe,
-            cpf: professional.cpf,
-            email: professional.email,
-            full_name: professional.fullName,
-            nickname: professional.nickname,
-            password_hash: hashedPassword,
-            phone_id: insertedPhone.id,
-            picture_name: pictureName,
-            workplace_id: insertedWorkplace.id
-          })
-          .returning('id');
-
-        await trx('service').insert(
-          services.map((service: ServiceType) => {
-            return {
-              estimated_duration: service.estimatedTime,
-              name: service.name,
-              price: service.startingPrice,
-              professional_id: insertedProfessional.id
-            };
-          })
-        );
-
-        await trx('workday').insert(
-          workHours.map((workHour: WorkHourType) => {
-            return {
-              break_time: false,
-              end_time: workHour.endTime,
-              professional_id: insertedProfessional.id,
-              start_time: workHour.startTime,
-              week_day: workHour.weekday
-            };
-          })
-        );
-
-        await trx.commit();
-      })
-      .then((_) => {
-        professional.picturePath = `${PICTURES_PATH}/${
-          pictureName || DEFAULT_USER_PICTURE
-        }`;
-
-        return res.status(201).json(professional);
-      })
-      .catch((err) => {
-        if (pictureName) {
-          fs.unlink(path.join(PICTURES_FOLDER, pictureName), (err) => {});
-        }
-        if (err.routine?.includes('unique')) {
-          const [_, key] = err.constraint.split('_');
-
-          return res.status(409).json({
-            error: `Já existe um usuário registrado com ${key} ${professional[key]}`
-          });
-        }
-
-        console.error(err);
-
-        return res
-          .status(500)
-          .json({ error: 'Erro ao inserir usuário no banco de dados' });
-      });
+      return internalError(res, 'Erro ao inserir usuário no banco de dados');
+    }
   }
 
   public static async login(req: Request, res: Response) {
     const loginCredentials = req.body;
 
-    const [professional] = await databaseService.connection
-      .table('professional')
-      .where('email', loginCredentials.email);
+    const professional = await ProfessionalController.getProfessionalByEmail(
+      loginCredentials.email
+    );
 
-    if (!professional)
-      return res.status(400).json({ error: 'Credenciais inválidas' });
+    if (!professional) return badRequest(res, 'Credenciais inválidas');
 
     const passwordsMatch = await bcrypt.compare(
       loginCredentials.password,
-      professional.password_hash
+      professional.passwordHash
     );
 
-    if (!passwordsMatch)
-      return res.status(400).json({ error: 'Credenciais inválidas' });
+    if (!passwordsMatch) return badRequest(res, 'Credenciais inválidas');
 
-    const [phone] = await databaseService.connection
-      .table('phone')
-      .where('id', professional.phone_id);
+    const { phone } = await PhoneController.getPhoneById(professional.phoneId);
+    professional.phone = phone;
 
-    const [workplace] = await databaseService.connection
-      .table('workplace')
-      .select(
-        'city',
-        'street',
-        'street_number',
-        'complement',
-        'phone1_id',
-        'phone2_id'
-      )
-      .where('id', professional.workplace_id);
-
-    const [[workplacePhone1], [workplacePhone2]] = await Promise.all([
-      databaseService.connection
-        .table('phone')
-        .select('phone', 'is_phone_whatsapp')
-        .where('id', workplace.phone1_id),
-      databaseService.connection
-        .table('phone')
-        .select('phone', 'is_phone_whatsapp')
-        .where('id', workplace.phone2_id)
-    ]);
-
-    delete workplace.phone1_id;
-    delete workplace.phone2_id;
-
-    workplace.phones = [];
-    workplace.phones.push(workplacePhone1);
-    if (workplacePhone2) workplace.phones.push(workplacePhone2);
-
-    const services = await databaseService.connection
-      .table('service')
-      .select('name', 'price', 'estimated_duration')
-      .where('professional_id', professional.id);
-
-    const workHours = await databaseService.connection
-      .table('workday')
-      .select('week_day', 'start_time', 'end_time', 'break_time')
-      .where('professional_id', professional.id);
-
-    const picturePath = `${PICTURES_PATH}/${
-      professional.picture_name || DEFAULT_USER_PICTURE
-    }`;
-
-    const accessToken = jwt.sign(
-      {
-        aboutMe: professional.about_me,
-        cpf: professional.cpf,
-        email: professional.email,
-        fullName: professional.full_name,
-        id: professional.id,
-        nickname: professional.nickname,
-        phone: phone.phone,
-        picturePath: picturePath,
-        services,
-        workHours,
-        workplace
-      },
-      process.env.JWT_LOGIN_SECRET,
-      { expiresIn: '2h' }
+    professional.workplace = await WorkplaceController.getWorkplaceById(
+      professional.workplaceId
     );
+
+    professional.services = await ServiceController.getServicesByProfessionalId(
+      professional.id
+    );
+
+    professional.workHours =
+      await WorkHourController.getWorkHoursByProfessionalId(professional.id);
+
+    professional.picturePath = userPicture(professional.pictureName);
+    professional.type = 'PROFESSIONAL';
+
+    delete professional.passwordHash;
+    delete professional.phoneId;
+    delete professional.pictureName;
+    delete professional.workplaceId;
+
+    const accessToken = jwt.sign(professional, process.env.JWT_LOGIN_SECRET, {
+      expiresIn: '2h'
+    });
 
     return res.status(200).json({ jwt: accessToken });
   }
@@ -238,26 +113,40 @@ export default class ProfessionalController {
   ) {
     const search = req.query;
 
-    const ids = (
-      await databaseService.connection
-        .select('professional.id')
-        .from('professional')
-        .join('workplace', 'professional.workplace_id', 'workplace.id')
-        .where('workplace.city', search.city)
-        .modify((queryBuilder) => {
-          if (search.query)
-            queryBuilder.whereILike(
-              'professional.full_name',
-              `%${search.query}%`
-            );
-        })
-    ).map((p: { id: number }) => p.id);
+    const ids = await ProfessionalController.findByCity(
+      search.city,
+      search.query
+    );
 
     const professionals = await ProfessionalController.getProfessionalsByIds(
       ids
     );
 
     res.status(200).json(professionals);
+  }
+
+  private static async findByCity(
+    city: string,
+    query?: string
+  ): Promise<number[]> {
+    return databaseService.connection
+      .select('professional.id')
+      .from('professional')
+      .join('workplace', 'professional.workplace_id', 'workplace.id')
+      .where('workplace.city', city)
+      .modify((queryBuilder) => {
+        if (query)
+          queryBuilder.whereILike('professional.full_name', `%${query}%`);
+      })
+      .then((rows) => rows.map((r: any) => r.id));
+  }
+
+  private static async getProfessionalByEmail(email: string): Promise<any> {
+    return databaseService.connection
+      .table('professional')
+      .where('email', email)
+      .first()
+      .then(toCamel);
   }
 
   private static async getProfessionalsByIds(ids: number[]) {
@@ -272,31 +161,78 @@ export default class ProfessionalController {
         'nickname',
         'picture_name'
       )
-      .whereIn('id', ids);
+      .whereIn('id', ids)
+      .then((rows) => rows.map(toCamel) as any[]);
 
     for (const p of professionals) {
-      p.fullName = p.full_name;
-      p.aboutMe = p.about_me;
-
-      delete p.full_name;
-      delete p.about_me;
-
       [{ phone: p.phone }] = await databaseService.connection
         .table('phone')
         .select('phone')
-        .where('id', p.phone_id);
-      delete p.phone_id;
+        .where('id', p.phoneId);
 
-      p.picturePath = userPicture(p.picture_name);
-      delete p.picture_name;
+      p.picturePath = userPicture(p.pictureName);
 
-      p.workplace = await WorkplaceController.getWorkplaceById(p.workplace_id);
-      delete p.workplace_id;
+      p.workplace = await WorkplaceController.getWorkplaceById(p.workplaceId);
 
       p.services = await ServiceController.getServicesByProfessionalId(p.id);
-      p.workHours = await WorkHoursController.getProfessionalWorkHours(p.id);
+      p.workHours = await WorkHourController.getWorkHoursByProfessionalId(p.id);
+
+      delete p.phoneId;
+      delete p.pictureName;
+      delete p.workplaceId;
     }
 
     return professionals;
+  }
+
+  private static async insertProfessional(professional: any) {
+    const localProfessional = JSON.parse(JSON.stringify(professional));
+
+    return databaseService.connection.transaction(async (trx) => {
+      const { workplace, services, workHours } = localProfessional;
+
+      workplace.phone1Id = await PhoneController.insertPhone(trx, {
+        phone: workplace.phone1,
+        isPhoneWhatsapp: workplace.isPhone1Whatsapp
+      });
+
+      delete workplace.phone1;
+      delete workplace.isPhone1Whatsapp;
+
+      if (workplace.phone2)
+        workplace.phone2Id = await PhoneController.insertPhone(trx, {
+          phone: workplace.phone2,
+          isPhoneWhatsapp: workplace.isPhone2Whatsapp
+        });
+
+      delete workplace.phone2;
+      delete workplace.isPhone2Whatsapp;
+
+      localProfessional.workplaceId = await WorkplaceController.insertWorkplace(
+        trx,
+        workplace
+      );
+
+      localProfessional.phoneId = await PhoneController.insertPhone(trx, {
+        phone: localProfessional.phone
+      });
+
+      delete localProfessional.phone;
+      delete localProfessional.workplace;
+      delete localProfessional.services;
+      delete localProfessional.workHours;
+
+      const [{ id: professionalId }] = await trx('professional')
+        .insert(toSnake(localProfessional))
+        .returning('id');
+
+      services.every((s: ServiceType) => (s.professionalId = professionalId));
+      await ServiceController.insertServices(trx, services);
+
+      workHours.every((w: WorkHourType) => (w.professionalId = professionalId));
+      await WorkHourController.insertWorkHours(trx, workHours);
+
+      await trx.commit();
+    });
   }
 }

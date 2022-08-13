08d4010572be695f7f68ca593f6c0f1054308b38
PauloUbirajara <ubirajara3@hotmail.com>
2022-08-12 23:31:44 -0600

    Remover teste e mock de requisição

--- backend/tests/middleware.test.ts
+++ backend/tests/middleware.test.ts
@@ -2,24 +2,6 @@ const { NextFunction } = require('express');
 const AuthGuardMiddleware = require('../src/middlewares/authGuard.middleware');
 const authGuard = AuthGuardMiddleware.default;
 
-function createMockRequest() {
-  const request = {
-    headers: {}
-  } as Express.Request;
-
-  const response = {
-    status: (statusCode: string) => {
-      this.status = statusCode;
-      return response;
-    },
-    json: (_message: string) => {}
-  } as Express.Response;
-
-  const next = [] as typeof NextFunction;
-
-  return [request, response, next];
-}
-
 describe('Auth Guard Middleware', () => {
   test('should fail if user type is invalid', () => {
     const invalidUserType = 'INVALID';
@@ -34,12 +16,5 @@ describe('Auth Guard Middleware', () => {
     const result = Promise.all(validPromises);
 
     expect(result).resolves;
-  });
-
-  test('should fail if unauthorized customer request', async () => {
-    const authMethod = await authGuard('CUSTOMER');
-    const unauthorizedRequest = authMethod(...createMockRequest());
-
-    expect(unauthorizedRequest).rejects;
   });
 });

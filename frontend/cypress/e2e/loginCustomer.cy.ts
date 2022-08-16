20d14e8e49d59bb54eeed222a12a618810dea91b
PauloUbirajara <ubirajara3@hotmail.com>
2022-08-15 19:22:47 -0600

    renaming cypress login customer test
    adding timeout before failing cypress test

--- frontend/cypress/e2e/loginCustomer.cy.ts
+++ frontend/cypress/e2e/loginCustomer.cy.ts
@@ -1,8 +1,10 @@
 import {} from 'cypress';
 
-describe('test login costumer', () => {
+describe('test login customer', () => {
   it('see if changed location', () => {
     cy.visit('http://localhost:3000/login');
+
+    cy.location('pathname', { timeout: 60000 }).should('include', '/login');
 
     cy.get('[data-cy=createAccountButton]').click();
     cy.url().should('eq', 'http://localhost:3000/cadastro');

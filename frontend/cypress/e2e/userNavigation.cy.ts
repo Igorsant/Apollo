568df810c5f4d48d5d6a52f358affc4631c5abbd
PauloUbirajara <ubirajara3@hotmail.com>
2022-08-18 23:34:56 -0600

    test:group test cases by 'customer' and 'professional' roles

--- frontend/cypress/e2e/userNavigation.cy.ts
+++ frontend/cypress/e2e/userNavigation.cy.ts
@@ -1,33 +1,37 @@
 import {} from 'cypress';
 
 describe('Validate visitor navigation in application', () => {
-  it('should pass if visitor can navigate to home page', () => {
-    cy.visit('http://localhost:3000');
-    cy.url().should('equal', 'http://localhost:3000/');
+  describe('Visitor as customer', () => {
+    it('should pass if visitor can navigate to home page', () => {
+      cy.visit('http://localhost:3000');
+      cy.url().should('equal', 'http://localhost:3000/');
+    });
+
+    it('should pass if visitor can navigate to login page', () => {
+      cy.visit('http://localhost:3000');
+      cy.get('[data-cy=loginAccountButton]').click();
+      cy.url().should('equal', 'http://localhost:3000/login');
+    });
+
+    it('should pass if visitor can navigate to register page', () => {
+      cy.visit('http://localhost:3000');
+      cy.get('[data-cy=createAccountButton]').click();
+      cy.url().should('equal', 'http://localhost:3000/cadastro');
+    });
   });
 
-  it('should pass if visitor can navigate to login page', () => {
-    cy.visit('http://localhost:3000');
-    cy.get('[data-cy=loginAccountButton]').click();
-    cy.url().should('equal', 'http://localhost:3000/login');
-  });
+  describe('Visitor as professional', () => {
+    it('should pass if visitor can navigate to professional login page', () => {
+      cy.visit('http://localhost:3000');
+      cy.get('[data-cy=professionalLoginButton]').click();
+      cy.url().should('equal', 'http://localhost:3000/profissional/login');
+    });
 
-  it('should pass if visitor can navigate to register page', () => {
-    cy.visit('http://localhost:3000');
-    cy.get('[data-cy=createAccountButton]').click();
-    cy.url().should('equal', 'http://localhost:3000/cadastro');
-  });
-
-  it('should pass if visitor can navigate to professional login page', () => {
-    cy.visit('http://localhost:3000');
-    cy.get('[data-cy=professionalLoginButton]').click();
-    cy.url().should('equal', 'http://localhost:3000/profissional/login');
-  });
-
-  it('should pass if visitor can navigate to professional register page', () => {
-    cy.visit('http://localhost:3000');
-    cy.get('[data-cy=professionalLoginButton]').click();
-    cy.get('[data-cy=professionalRegisterButton]').click();
-    cy.url().should('equal', 'http://localhost:3000/profissional/cadastro');
+    it('should pass if visitor can navigate to professional register page', () => {
+      cy.visit('http://localhost:3000');
+      cy.get('[data-cy=professionalLoginButton]').click();
+      cy.get('[data-cy=professionalRegisterButton]').click();
+      cy.url().should('equal', 'http://localhost:3000/profissional/cadastro');
+    });
   });
 });

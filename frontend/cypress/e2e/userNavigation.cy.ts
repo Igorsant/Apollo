12a14b4dd61a0b3900c617c4de5f409c2c46d8ef
PauloUbirajara <ubirajara3@hotmail.com>
2022-08-18 23:22:41 -0600

    test:add visitor navigation validation

---
+++ frontend/cypress/e2e/userNavigation.cy.ts
@@ -0,0 +1,20 @@
+import {} from 'cypress';
+
+describe('Validate visitor navigation in application', () => {
+  it('should pass if visitor can navigate to home page', () => {
+    cy.visit('http://localhost:3000');
+    cy.url().should('equal', 'http://localhost:3000/');
+  });
+
+  it('should pass if visitor can navigate to login page', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('[data-cy=loginAccountButton]').click();
+    cy.url().should('equal', 'http://localhost:3000/login');
+  });
+
+  it('should pass if visitor can navigate to register page', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('[data-cy=createAccountButton]').click();
+    cy.url().should('equal', 'http://localhost:3000/cadastro');
+  });
+});

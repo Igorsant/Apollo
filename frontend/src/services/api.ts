9e70348f28ab462c3f05759174bb4be1b721d56d
PauloUbirajara <ubirajara3@hotmail.com>
2022-09-07 17:44:19 -0600

    Preencher cabeçalho permitindo acesso à outros sites

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -5,6 +5,8 @@ import { errorHandler } from './errorHandler';
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Methods': '*',
     'Access-Control-Allow-Origin': '*'
   }
 });

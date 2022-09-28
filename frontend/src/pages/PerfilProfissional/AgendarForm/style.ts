4b1794a8e0efc0ec8760ae76e11ce98938f2cee8
PauloUbirajara <ubirajara3@hotmail.com>
2022-09-28 02:24:03 -0300

    Adicionar arquivo de styled components usados em formulário de agendamento

---
+++ frontend/src/pages/PerfilProfissional/AgendarForm/style.ts
@@ -0,0 +1,80 @@
+import { Button } from '../../../components/Button/ApolloButton';
+import { Close } from '@mui/icons-material';
+import styled from 'styled-components';
+
+export const AgendarModalBackground = styled.div`
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.4);
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  left: 0px;
+  position: fixed;
+  top: 0px;
+  width: 100vw;
+`;
+
+export const AgendarModal = styled.div`
+  align-items: center;
+  background-color: white;
+  border-radius: 5px;
+  color: black;
+  display: grid;
+  gap: 12px;
+  grid-template-areas:
+    'close'
+    'title'
+    'steps'
+    'buttons';
+  justify-content: center;
+  padding: 15px;
+`;
+
+export const ModalCloseButton = styled(Close)`
+  align-self: end;
+  border-radius: 50%;
+  color: var(--header);
+  cursor: pointer;
+  display: flex;
+  grid-area: close;
+  margin-left: auto;
+  padding: 5px;
+  &:hover {
+    background: rgba(0, 0, 0, 0.03);
+  }
+`;
+
+export const ModalTitle = styled.h2`
+  font-weight: bold;
+  display: block;
+  text-align: center;
+  grid-area: title;
+`;
+
+export const ModalStepProgress = styled.div`
+  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr auto 1fr auto 1fr auto 1fr;
+  justify-content: center;
+  text-align: center;
+`;
+
+export const ModalCurrentStep = styled.div`
+  grid-area: steps;
+`;
+
+export const ModalUserButtons = styled.div`
+  display: grid;
+  gap: 15px;
+  grid-area: buttons;
+  grid-template-areas: 'previous next';
+`;
+
+export const ModalNextStepButton = styled(Button)`
+  grid-area: next;
+`;
+
+export const ModalPreviousStepButton = styled(Button)`
+  background-color: #cd3838 !important; /* Sobrescrever o estilo de tema do Button */
+  grid-area: previous;
+`;

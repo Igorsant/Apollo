3fd4adfd7a529e8d0cb10a4f3fade10b32d6c040
PauloUbirajara <ubirajara3@hotmail.com>
2022-09-28 14:25:33 -0300

    Ajustar estilização de separatórias entre steps de progresso

--- frontend/src/pages/PerfilProfissional/AgendarForm/line.tsx
+++ frontend/src/pages/PerfilProfissional/AgendarForm/line.tsx
@@ -1,13 +1,11 @@
 import styled from 'styled-components';
 
 export const Line = styled.div`
-  width: 50px;
-  height: 1px;
   background-color: gray;
+  height: 2px;
 `;
 
 export const ActiveLine = styled.div`
-  width: 50px;
-  height: 1px;
   background-color: var(--header);
+  height: 2px;
 `;

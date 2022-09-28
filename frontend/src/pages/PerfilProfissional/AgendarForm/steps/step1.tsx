1befeec3ce0f4819a3cbcb4ceb674fbecd62b795
PauloUbirajara <ubirajara3@hotmail.com>
2022-09-28 14:23:29 -0300

    Ajustar estilização para não colidir com espaçamento de AgendarModal

--- frontend/src/pages/PerfilProfissional/AgendarForm/steps/step1.tsx
+++ frontend/src/pages/PerfilProfissional/AgendarForm/steps/step1.tsx
@@ -1,4 +1,4 @@
-import { ArrowDropDown } from '@mui/icons-material';
+import { ArrowRight } from '@mui/icons-material';
 import { ServicesButton } from '../../style';
 import { ServiceType } from '..';
 import { SetStateAction, Dispatch } from 'react';
@@ -22,15 +22,18 @@ export const Step1 = ({
   <>
     <div
       style={{
+        alignItems: 'center',
         backgroundColor: 'var(--input)',
         color: 'var(--header)',
         display: 'flex',
-        alignItems: 'center',
-        width: 'auto',
-        marginTop: '50px'
+        gap: '10px',
+        padding: '10px',
+        textAlign: 'center',
+        whiteSpace: 'nowrap',
+        width: 'auto'
       }}
     >
-      <ArrowDropDown style={{ fontSize: '3em' }} />
+      <ArrowRight style={{ fontSize: '2rem' }} />
       <h2>Selecione serviços</h2>
       {servicesAvailable.map((service) => (
         <ServicesButton
@@ -44,29 +47,48 @@ export const Step1 = ({
         </ServicesButton>
       ))}
     </div>
-    <div style={{ color: 'var(--header)', marginTop: '15px', display: 'flex' }}>
-      <h4>Serviços Selecionados:</h4>
-      {choosenServices.map((service) => (
-        <ServicesButton
-          key={service.id}
-          onClick={() => {
-            setServicesAvailable((curr) => [...curr, service]);
-            setChoosenServices((curr) => curr.filter((val) => val.id !== service.id));
-          }}
-        >
-          {service.name}
-        </ServicesButton>
-      ))}
+    <div
+      style={{
+        alignItems: 'start',
+        color: 'var(--header)',
+        display: 'grid',
+        gridTemplateColumns: 'auto 1fr',
+        marginTop: '1rem',
+        padding: '10px 0'
+      }}
+    >
+      {choosenServices.length > 0 && <h4>Serviços Selecionados:</h4>}
+      <div
+        style={{
+          alignItems: 'start',
+          display: 'grid',
+          gap: '10px',
+          gridTemplateColumns: 'repeat(auto-fit, minmax(min-content, 15%))',
+          padding: '0 10px',
+          whiteSpace: 'nowrap'
+        }}
+      >
+        {choosenServices.map((service) => (
+          <ServicesButton
+            key={service.id}
+            onClick={() => {
+              setServicesAvailable((curr) => [...curr, service]);
+              setChoosenServices((curr) => curr.filter((val) => val.id !== service.id));
+            }}
+          >
+            {service.name}
+          </ServicesButton>
+        ))}
+      </div>
     </div>
-    <h2 style={{ marginTop: '100px', color: 'var(--header)' }}>
-      Tempo total estimado:
+    <h2 style={{ color: 'var(--header)', marginTop: '10px' }}>
       {choosenServices.length > 0 &&
-        choosenServices
+        `Tempo total estimado: ${choosenServices
           .map((s) => Number.parseInt(s.time))
           .reduce((acc, next) => {
             return acc + next;
-          }, 0)}{' '}
-      min.
+          }, 0)}
+       min.`}
     </h2>
   </>
 );
